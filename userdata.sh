e649d7c4ff02162c1c11e6c89605c3f3602be83c
“ekvt98” <evitateka@outlook.fr>
2026-05-18 13:43:18 +0200

    userdata changes

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "<html><h1><p> Welcome to Utrains the place to learn DevOps , Cloud , Linux!!.<br> This traffic is servered from:  ${HOSTNAME} </p></h1></html>" > /var/www/html/index.html
+echo "<html><h1><p> Welcome to my httpd website !.<br> This traffic is servered from:  ${HOSTNAME} </p></h1></html>" > /var/www/html/index.html
